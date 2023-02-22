b5d15aa03b70585dacfad68dc4116d72d000992d
Fmadhi313 <Fmadi@alfaisal.edu>
2023-02-22 21:47:04 +0300

    nothing

--- lib/widgets/homeWidget/aboutusWidget.dart
+++ lib/widgets/homeWidget/aboutusWidget.dart
@@ -10,25 +10,6 @@ class AboutUs extends StatelessWidget {
     return Stack(
       children: [
         // 3 containers, one for the image and two for the text
-        Container(
-          height: 200,
-          width: 200,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/regalMensed7.png'),
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-        Container(
-          height: 500,
-          width: double.infinity,
-          color: Colors.white,
-          child: const Text(
-            'About Us',
-            style: WebTextStyles.home_widgets_Text,
-          ),
-        ),
       ],
     );
   }

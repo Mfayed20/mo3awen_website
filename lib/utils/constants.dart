8610cc06837f90312cd0ddb59fb261434223ea51
Fmadhi313 <Fmadi@alfaisal.edu>
2023-03-01 21:04:08 +0300

    new footer

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -5,3 +5,11 @@ import 'package:mo3awen_website/utils/styles.dart';
 
 const String navBarLogo = 'assets/page-1/images/logo-navigation-bar-f7b.png';
 const String welcomeHomePage = 'assets/page-1/images/hero-section-image-bg.png';
+const String footerlogoimg = 'assets/page-1/images/vector-733.png';
+const String footerlogoimglatter_M = 'assets/page-1/images/vector-Ya9.png';
+const String footerlogoimglatter_O = 'assets/page-1/images/vector-rQ5.png';
+const String footerlogoimglatter_3 = 'assets/page-1/images/vector-Fkh.png';
+const String footerlogoimglatter_A = 'assets/page-1/images/vector-Hss.png';
+const String footerlogoimglatter_W = 'assets/page-1/images/vector.png';
+const String footerlogoimglatter_E = 'assets/page-1/images/vector-s41.png';
+const String footerlogoimglatter_N = 'assets/page-1/images/vector-guK.png';

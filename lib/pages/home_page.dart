bd2039f04ad95b1be1bc0cab450087506694a4c8
mfayed <Fayed-Mid-2-Practice>
2023-03-06 18:46:16 +0300

    rename home_widget to home_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 import '../widgets/footer_widget.dart';
-import '../widgets/homeWidget/plans_widget.dart';
-import '../widgets/homeWidget/visit_us_widget.dart';
-import '../widgets/homeWidget/welcome_widget.dart';
+import '../widgets/homePage/plans_widget.dart';
+import '../widgets/homePage/visit_us_widget.dart';
+import '../widgets/homePage/welcome_widget.dart';
 import '../widgets/nav_bar.dart';
-import '../widgets/homeWidget/about_us_widget.dart';
+import '../widgets/homePage/about_us_widget.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
